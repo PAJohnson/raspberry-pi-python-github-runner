9aefeab3fe5560e3b2ced89ebeeac7b84125d6be
PAJohnson <pajohns4@ncsu.edu>
2021-01-07 18:32:28 -0500

    try again

--- src/test.py
+++ src/test.py
@@ -1,8 +1,5 @@
 from datetime import datetime
-import os
 
 if __name__ == "__main__":
-    print(os.path.abspath(os.path.dirname(__file__)))
-    with open(os.path.abspath(os.path.dirname(__file__)) + '/../artifacts/output.txt','w') as outfile:
-        outfile.write("Hello world! GH Action Artifact")
-        outfile.write(datetime.now().strftime("%b-%d-%Y-%H:%M:%S"))
+    print("Hello world! GH Action Artifact")
+    print(datetime.now().strftime("%b-%d-%Y-%H:%M:%S"))
