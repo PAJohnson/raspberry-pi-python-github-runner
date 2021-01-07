f9d29a2339fe424d13f26ecbcefa0a4b65fda5ae
PAJohnson <pajohns4@ncsu.edu>
2021-01-07 18:22:03 -0500

    one mo 'gain

--- src/test.py
+++ src/test.py
@@ -4,6 +4,6 @@ import pathlib
 
 if __name__ == "__main__":
     print(pathlib.Path(__file__).parent.absolute())
-    with open('artifacts/output.txt','w') as outfile:
+    with open('../artifacts/output.txt','w') as outfile:
         outfile.write("Hello world! GH Action Artifact")
         outfile.write(datetime.now().strftime("%b-%d-%Y-%H:%M:%S"))
