6096a890dab97eb4d01d734599d372e89ad55f84
PAJohnson <pajohns4@ncsu.edu>
2021-01-07 18:20:25 -0500

    try again

--- src/test.py
+++ src/test.py
@@ -1,6 +1,9 @@
 from datetime import datetime
+import pathlib
+
 
 if __name__ == "__main__":
+    print(pathlib.Path(__file__).parent.absolute())
     with open('artifacts/output.txt','w') as outfile:
         outfile.write("Hello world! GH Action Artifact")
         outfile.write(datetime.now().strftime("%b-%d-%Y-%H:%M:%S"))
